1c4546a449f17340722ecb909c31a7532e25f4f7
Jian Huang <jianh2@Nycstudents.net>
2022-11-16 09:29:19 -0500

    U4T4Lab

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int row = 1; row <= 6; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print(row * col + " " );
+
+
+            }
+            System.out.println();
+
+
+        }
+    }
+}
